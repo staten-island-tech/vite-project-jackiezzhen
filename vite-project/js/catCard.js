879d97e2918ec1989c4ca30b8d04b8243667a799
jackiezzhen <jackiezhen31@gmail.com>
2023-11-27 23:38:52 -0500

    evolution button cycling finish

--- vite-project/js/catCard.js
+++ vite-project/js/catCard.js
@@ -1,75 +1,46 @@
 function catCard(cat) {
   const cardHTML = `
     <div class="newcard">
-      <img class="catImg" id="baseImg" src="img/${cat.baseUrl}.webp" alt="Image/Error">
-      <img class="catImg" id="evolvedImg" src="img/${cat.evolvedUrl}.webp" alt="Image/Error">
-      <img class="catImg" id="trueImg" src="img/${cat.trueUrl}.webp" alt="Image/Error">
-      <div class="imgbtn">
-        <button id="basebutton">${cat.base}</button>
-        <button id="evolvedbutton">${cat.evolved}</button>
-        ${cat.true != null ? `<button id="truebutton">${cat.true}</button>` : ''}
+      <div class="catImg">
+        <img class="baseImg" src="img/${cat.baseUrl}.webp" alt="Image/Error">
+        <img class="evolvedImg" src="img/${cat.evolvedUrl}.webp" alt="Image/Error">
+        <img class="trueImg" src="img/${cat.trueUrl}.webp" alt="Image/Error">
       </div>
-    
-    
+      <div class="imgbtn">
+        <button class="basebutton">${cat.base}</button>
+        <button class="evolvedbutton">${cat.evolved}</button>
+        ${cat.true != null ? `<button class="truebutton">${cat.true}</button>` : ''}
+      </div>
+    </div>
     <style>
-      #evolvedImg, #trueImg {
+      .evolvedImg, .trueImg {
         display: none;
       }
     </style>
-    
-    <script>
-    const imgDOM = {
-      baseImg: document.getElementById("baseImg");
-      evolvedImg: document.getElementById("evolvedImg");
-      trueImg: document.getElementById("trueImg");
-      }
-    imgDOM.baseImg.addEventListener("click", () => {
-      imgDOM.baseImg.style.display = "inline-block";
-      imgDOM.evolvedImg.style.display = "none";
-      imgDOM.trueImg.style.display = "none";
-    });
-  
-    imgDOM.evolvedImg.addEventListener("click", () => {
-      imgDOM.baseImg.style.display = "none";
-      imgDOM.evolvedImg.style.display = "inline-block";
-      imgDOM.trueImg.style.display = "none";
-    });
-  
-    imgDOM.trueImg.addEventListener("click", () => {
-      imgDOM.baseImg.style.display = "none";
-      imgDOM.evolvedImg.style.display = "none";
-      imgDOM.trueImg.style.display = "inline-block";
-      });
-      </script>
-      </div>
   `;
 
   document.querySelector('.container').insertAdjacentHTML('beforeend', cardHTML);
-/*
-  const imgDOM = {
-  baseImg: document.getElementById("baseImg");
-  evolvedImg: document.getElementById("evolvedImg");
-  trueImg: document.getElementById("trueImg");
-  }
+}
 
-  document.getElementById("basebutton").addEventListener("click", () => {
-    baseImg.style.display = "inline-block";
-    evolvedImg.style.display = "none";
-    trueImg.style.display = "none";
-  });
+document.addEventListener("click", (event) => {
+  const card = event.target.closest('.newcard');
+  const baseImg = card.querySelector('.baseImg');
+  const evolvedImg = card.querySelector('.evolvedImg');
+  const trueImg = card.querySelector('.trueImg');
 
-  document.getElementById("evolvedbutton").addEventListener("click", () => {
-    baseImg.style.display = "none";
-    evolvedImg.style.display = "inline-block";
-    trueImg.style.display = "none";
-  });
-
-  document.getElementById("truebutton").addEventListener("click", () => {
+    if (event.target.classList.contains("basebutton")) {
+      baseImg.style.display = "inline-block";
+      evolvedImg.style.display = "none";
+      trueImg.style.display = "none";
+    } else if (event.target.classList.contains("evolvedbutton")) {
+      baseImg.style.display = "none";
+      evolvedImg.style.display = "inline-block";
+      trueImg.style.display = "none";
+    } else if (event.target.classList.contains("truebutton")) {
       baseImg.style.display = "none";
       evolvedImg.style.display = "none";
       trueImg.style.display = "inline-block";
-    });
-    */
+    }
   }
-
+);
 export { catCard };
